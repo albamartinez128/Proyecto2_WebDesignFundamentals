4f0536cc670b495341b2babc6469324dd5eadc84
albamartinez128 <albamartinez128@gmail.com>
2023-12-14 11:11:49 +0100

    first commit

---
+++ data/data.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    name: "Iphone 15 Pro",
+    price: 1219,
+    seller: "PcComponentes",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777839/140-apple-iphone-15-pro-128gb-titanio-natural-libre.jpg",
+  },
+
+  {
+    name: "Iphone 14",
+    price: 919,
+    seller: "PcComponentes",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1058/10581410/120-apple-iphone-14-256gb-medianoche-libre.jpg",
+  },
+  {
+    name: "Nilox NXM24FHD1441 24",
+    price: 124.99,
+    seller: "PcComponentes",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1033/10334393/1451-nilox-nxm24fhd1441-24-led-fullhd-165hz-g-sync-compatible.jpg",
+  },
+  {
+    name: "Samsung Smart Monitor 32",
+    price: 344,
+    seller: "infopavon",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1067/10672796/1947-samsung-smart-monitor-m7-ls32bm701upxen-32-led-ultrahd-4k-smart.jpg",
+  },
+  {
+    name: "Acer Nitro QG240YS3 23.8",
+    price: 119,
+    seller: "infopavon",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1072/10722543/111-acer-nitro-qg240ys3-238-led-fullhd-180hz-freesync-review.jpg",
+  },
+  {
+    name: "Apple Macbook Air 2023 ",
+    price: 1649.0,
+    seller: "infopavon",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1073/10736229/1255-apple-macbook-air-2023-apple-m2-8gb-512gb-ssd-gpu-deca-core-153-gris-espacial.jpg",
+  },
+  {
+    name: "Apple Airpods",
+    price: 125.0,
+    seller: "PcComponentes",
+    image: "https://thumb.pccomponentes.com/w-530-530/articles/19/192582/2.jpg",
+  },
+
+  {
+    name: "TD Systems PRIME24C14S 24",
+    price: 108.99,
+    seller: "TD Systems Official",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1079/10795761/122-td-systems-prime24c14s-24-led-hd-ready-smart-tv.jpg",
+  },
+  {
+    name: "HP 255 G8 AMD Ryzen 15.6",
+    price: 588.5,
+    seller: "Ocasionia",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1065/10658138/175-hp-255-g8-amd-ryzen-3-5300u-8gb-1tb-ssd-156.jpg",
+  },
+  {
+    name: "HP 250 G8 Intel Celeron15.6",
+    price: 460.78,
+    seller: "Ocasionia",
+    image:
+      "https://thumb.pccomponentes.com/w-530-530/articles/1002/10023592/1334-hp-255-g8-amd-ryzen-5-3500u-8-gb-256gbssd-156.jpg",
+  },
+];
+
+export default products;
---
+++ index.js
@@ -0,0 +1,174 @@
+import products from "./data/data.js";
+
+/**
+ AÑADIR ELEMENTOS AL BODY 
+ */
+const addHtmlContent = (child) => {
+  document.body.appendChild(child);
+};
+/**
+ CREACIÓN HEADER
+ */
+
+const addHTML = (header) => {
+  document.body.innerHTML += header;
+};
+
+addHTML(`
+  <header class="header">
+    <img class="logo" src= "./images/logo1.jpg"></img>
+    <nav class="list_item">
+      <ul >
+        <li><a href='#'>HOME</a></li>
+        <li><a href='#ABOUT'>ABOUT</a></li>
+        <li><a href='#CONTACT'>CONTACT</a></li>
+      </ul>
+    </nav>
+  </header>
+`);
+
+/**
+CREACIÓN DE UNA TARJETA DE PRODUCTOS USANDO TAGTEMPLATE - DOM
+ */
+
+const displayProduct = (product) => {
+  const container = document.createElement("section");
+  container.classList.add("container");
+  // container.setAttribute("id", "productSection");
+  const productElement = document.createElement("article");
+
+  productElement.classList.add("card");
+
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("card_img");
+
+  const productImage = document.createElement("img");
+  productImage.src = product.image;
+  productImage.alt = product.name;
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("card_content");
+
+  const productName = document.createElement("h3");
+  productName.textContent = product.name;
+
+  const productSeller = document.createElement("h4");
+  productSeller.textContent = `${product.seller}`;
+
+  const productPrice = document.createElement("p");
+  productPrice.textContent = `${product.price} €`;
+
+  imgContainer.appendChild(productImage);
+  textContainer.appendChild(productName);
+  textContainer.appendChild(productSeller);
+  textContainer.appendChild(productPrice);
+
+  productElement.appendChild(imgContainer);
+  productElement.appendChild(textContainer);
+
+  return productElement;
+};
+
+const productSection = document.createElement("section");
+productSection.classList.add("container");
+products.forEach((product) => {
+  productSection.appendChild(displayProduct(product));
+});
+
+addHTML(`
+<div id="filter-options">
+  <div id="filter_sellers">
+    <select id="seller-options">
+      <option value="">-- SELECT SELLER --</option>
+      <option value="PcComponentes">PcComponentes</option>
+      <option value="infopavon">infopavon</option>
+      <option value="TD Systems Official">TD Systems Official</option>
+      <option value="Ocasionia">Ocasionia</option>
+    </select>
+  </div>
+  <div id="filter-buttons">
+    <input id="price" type="number" min="25" placeholder="Price" />
+    <button id="button-search">Search</button>
+    <button id="button-reset">Reset</button>
+  </div>
+</div>
+
+`);
+
+addHtmlContent(productSection);
+
+addHTML(
+  `<footer class="footer">
+  <p>&copy; 2023 Alba Martínez</p><br>
+  <h4>Create in ThePower Business School</h4
+  `
+);
+
+/**APLICACIÓN DE FILTROS SELLER */
+let currentSeller = "";
+
+const filterSeller = (selectedSeller) => {
+  const filterProducts = products.filter((product) => {
+    if (selectedSeller === "") {
+      return true;
+    } else {
+      currentSeller = selectedSeller;
+      return product.seller === selectedSeller;
+    }
+  });
+
+  const productSection = document.querySelector(".container");
+  productSection.innerHTML = "";
+  filterProducts.forEach((product) => {
+    productSection.appendChild(displayProduct(product));
+  });
+};
+
+const filterPrice = (price) => {
+  const filteredProducts = products.filter((product) => {
+    if (currentSeller === "") return product.price <= price;
+    return product.seller === currentSeller && product.price <= price;
+  });
+
+  const productSection = document.querySelector(".container");
+  productSection.innerHTML = "";
+
+  if (filteredProducts.length === 0) {
+    productSection.innerHTML = `<h3>There is no products with this price</h3>`;
+  } else {
+    filteredProducts.forEach((product) => {
+      productSection.appendChild(displayProduct(product));
+    });
+  }
+};
+
+// Options to select by seller
+const sellerOptions = document.querySelector("#seller-options");
+
+sellerOptions.addEventListener("change", () => {
+  const selectedSeller = document.querySelector("#seller-options").value;
+  filterSeller(selectedSeller);
+});
+
+/**APLICACION FILTRO PRICE */
+
+// Button to search by price
+const searchbutton = document.querySelector("#button-search");
+
+searchbutton.addEventListener("click", () => {
+  const selectedPrice = Number(document.querySelector("#price").value);
+
+  filterPrice(selectedPrice);
+});
+
+//Reset
+
+const resetbutton = document.querySelector("#button-reset");
+resetbutton.addEventListener("click", () => {
+  document.querySelector("#price").value = "";
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  products.forEach((product) => {
+    container.appendChild(displayProduct(product));
+  });
+});
